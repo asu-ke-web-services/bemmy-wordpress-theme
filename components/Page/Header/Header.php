2552e0294ce46eeab46faff443e0da23182a988b
Ivan Montiel <idmontie@asu.edu>
2016-01-07 16:45:28 -0700

    Work on visuals

--- components/Page/Header/Header.php
+++ components/Page/Header/Header.php
@@ -1,10 +1,18 @@
 <?php
 
 function Page_Header() {
+  if ( has_post_thumbnail() ) {
+    //Article_Feature();
+  } else {
 ?>
-<header class="article__header">
-  <?php the_title( '<h1 class="article__title">', '</h1>' ); ?>
-</header><!-- .entry-header -->
+    <header class="article__header">
+      <?php the_title( '<h1 class="article__title">', '</h1>' ); ?>
+    </header><!-- .entry-header -->
+<?php
+  }
+
+?>
+
 
 <?php
 }
