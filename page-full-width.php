ec0315141288feb71844aa678401f270bb0ac1c6
Ivan Montiel <idmontie@asu.edu>
2016-01-08 13:48:54 -0700

    Full page template

---
+++ page-full-width.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Bemmy
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="page">
+    <main id="main" class="page__main page__main--full-width" role="main">
+
+      <?php
+      while ( have_posts() ) : the_post();
+
+        get_template_part( 'template-parts/content', 'page' );
+
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+          comments_template();
+        endif;
+
+      endwhile; // End of the loop.
+      ?>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
